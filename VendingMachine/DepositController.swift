2d952c99a7f5264f1677eaefb3b05e6d08f18899
buikevin96 <buikevin96@gmail.com>
2016-09-18 01:14:39 -0400

    Created comments for initial commit

--- VendingMachine/DepositController.swift
+++ VendingMachine/DepositController.swift
@@ -3,6 +3,7 @@
 //  VendingMachine
 //
 //  Created by Kevin Bui on 5/27/16.
+//  Edited by Kevin Bui on 9/18/16
 //  Copyright © 2016 Treehouse. All rights reserved.
 //
 
--- VendingMachine/ViewController.swift
+++ VendingMachine/ViewController.swift
@@ -3,6 +3,7 @@
 //  VendingMachine
 //
 //  Created by Pasan Premaratne on 1/19/16.
+//  Edited by Kevin Bui on 9/18/16
 //  Copyright © 2016 Treehouse. All rights reserved.
 //
 
